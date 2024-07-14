2c384920592a6288b76e5d039395b335cc6d8978
Anna Sheina <Eryholme39@gmail.com>
2024-07-14 15:36:17 +0300

    Домашнее задание к 10-ой лекции

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,25 +1,39 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FactorialTest {
     @Test
-    public void zeroFactorialTest(){
-        assertEquals(1,Factorial.countFactorial(0));
+    @DisplayName("Проверка нуля")
+    public void zeroFactorialTest() {
+        assertEquals(1, Factorial.countFactorial(0));
     }
-    @Test
-    public void positiveFactorialTest(){
-        assertEquals(1,Factorial.countFactorial(1));
-        assertEquals(24,Factorial.countFactorial(4));
-        assertEquals(120,Factorial.countFactorial(5));
+
+    @DisplayName("Проверка позитивных значений")
+    @ParameterizedTest
+    @CsvSource({
+            "1,1",
+            "2,2",
+            "3,6",
+            "4,24",
+            "5,120"
+    })
+    public void positiveFactorialTest(int m, int expected) {
+        int actual = Factorial.countFactorial(m);
+        assertEquals(expected, actual);
     }
+
     @Test
-    public void negativeFactorialTest(){
-        assertThrows(IllegalArgumentException.class,()->Factorial.countFactorial(-1));
+    @DisplayName("Проверка негативного значения")
+    public void negativeFactorialTest() {
+        assertThrows(IllegalArgumentException.class, () -> Factorial.countFactorial(-1));
     }
 }
